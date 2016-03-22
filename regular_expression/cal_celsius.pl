8eab2a111d41a0e95a9c7480d53c90d6dd1c8d19
zengzhaohu <zengzhaohu@innovem.com.cn>
2016-03-22 21:06:40 +0800

    正则学习笔记

--- regular_expression/cal_celsius.pl
+++ regular_expression/cal_celsius.pl
@@ -1,4 +1,12 @@
-$celsius = 30;
-$fahrenheit = ($celsius * 9 / 5) + 32;
-print "$celsius C is $fahrenheit F.\n";
-
+$celsius = <STDIN>;
+chomp($celsius);
+#匹配正则 m/.../
+#比较数值: == , =～ 连接正则表达式和待搜索的目标字符串
+#if ($celsius =~ m/^[0-9]+$/) {
+#(?:) 只分组不捕获
+if ($celsius =~ m/^[-+]?[0-9]+(\.[0-9]*)?$/) {
+    $fahrenheit = ($celsius * 9 / 5) + 32;
+    print "$celsius C is $fahrenheit F.\n";
+} else {
+    print "not only digits\n";
+}
