360c7de72f6c60fa72ef5b64a978a022f0fe5806
zzh <git_zzh@163.com>
2016-04-17 10:28:29 +0800

    sudoku basic demo

---
+++ muduo_demo/sudoku/sudoku_server.cpp
@@ -0,0 +1,119 @@
+/*******************************************************************************
+Copyright: 2015, Innovem Tech. Co., Ltd.
+FileName: sudoku_server.cpp
+*******************************************************************************/
+#include "sudoku.h"
+
+#include <muduo/base/Atomic.h>
+#include <muduo/base/Logging.h>
+#include <muduo/base/Thread.h>
+#include <muduo/net/EventLoop.h>
+#include <muduo/net/InetAddress.h>
+#include <muduo/net/TcpServer.h>
+
+#include <functional>
+
+#include <utility>
+
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace muduo;
+using namespace muduo::net;
+
+class SudokuServer
+{
+    public:
+        SudokuServer(EventLoop *loop, const InetAddress &listenAddr)
+            : m_Server(loop, listenAddr, "SudokuServer"),
+            m_StartTime(Timestamp::now())
+    {
+        m_Server.setConnectionCallback(std::bind(&SudokuServer::onConnection, this, std::placeholders::_1));
+        m_Server.setMessageCallback(std::bind(&SudokuServer::onMessage, this,
+                    std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+    }
+        void start()
+        {
+            m_Server.start();
+        }
+    private:
+        void onConnection(const TcpConnectionPtr &conn)
+        {
+            LOG_TRACE << conn->peerAddress().toIpPort() << " -> "
+                << conn->localAddress().toIpPort() << " is "
+                << (conn->connected() ? "UP" : "DOWN");
+        }
+
+        void onMessage(const TcpConnectionPtr &conn, Buffer *buf, Timestamp)
+        {
+            LOG_DEBUG << conn->name();
+
+            size_t len = buf->readableBytes();
+            while (len >= kCells + 2) {
+                const char *crlf = buf->findCRLF();
+                if (crlf) {
+                    string request(buf->peek(), crlf);
+                    buf->retrieveUntil(crlf + 2);
+                    len = buf->readableBytes();
+                    if (!processRequest(conn, request)) {
+                        conn->send("Send Request!\r\n");
+                        conn->shutdown();
+                        break;
+                    }
+                } else if (len > 100) {
+                    conn->send("Id too long!\r\n");
+                    conn->shutdown();
+                    break;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        bool processRequest(const TcpConnectionPtr &conn, const string &request)
+        {
+            string id;
+            string puzzle;
+            bool goodRequest = true;
+
+            auto  colon = find(request.begin(), request.end(), ":");
+            if (true) {
+            //if (colon != request.end()) {
+                id.assign(request.begin(), colon);
+                puzzle.assign(colon+1, request.end());
+            } else {
+                puzzle = request;
+            }
+
+            if (puzzle.size() == implicit_cast<size_t>(kCells)) {
+                LOG_DEBUG << conn->name();
+                string result = solveSudoku(puzzle);
+                if (id.empty()) {
+                    conn->send(result + "\r\n");
+                } else {
+                    conn->send(id + ":" + result + "\r\n");
+                }
+            } else {
+                goodRequest = false;
+            }
+
+            return goodRequest;
+        }
+
+        TcpServer m_Server;
+        Timestamp m_StartTime;
+};
+
+int main()
+{
+    LOG_INFO << "pid = " << getpid() << ", tid = " << CurrentThread::tid();
+    EventLoop loop;
+    InetAddress listenAddr(9981);
+    SudokuServer server(&loop, listenAddr);
+    server.start();
+    loop.loop();
+    return 0;
+}
+
+
+
